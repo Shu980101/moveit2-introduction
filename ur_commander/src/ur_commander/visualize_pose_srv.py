a2cce15d74858bd587c77f66fcf8283b0e6be607
huanyu <91276231+HuanyuL@users.noreply.github.com>
2024-11-15 13:41:02 +0000

    update notebook

--- ur_commander/src/ur_commander/visualize_pose_srv.py
+++ ur_commander/src/ur_commander/visualize_pose_srv.py
@@ -1,5 +1,4 @@
 import rclpy
-
 from rclpy.node import Node
 from rclpy.qos import (
     QoSDurabilityPolicy,
@@ -8,25 +7,44 @@ from rclpy.qos import (
     QoSReliabilityPolicy,
 )
 from geometry_msgs.msg import PoseArray
+from ur_commander.srv import VisualizePoses
 
 
-class VisualizePoseSrv:
+class VisualizePoseSrv(Node):
     def __init__(self):
         super().__init__("visualize_pose_srv")
-        node = Node("visualize_pose_srv")
-        self.publisher = node.create_publisher(
+        self.publisher = self.create_publisher(
             msg_type=PoseArray,
             topic="/pose_visualizer",
             qos_profile=QoSProfile(
                 durability=QoSDurabilityPolicy.VOLATILE,
                 reliability=QoSReliabilityPolicy.BEST_EFFORT,
                 history=QoSHistoryPolicy.KEEP_LAST,
-                depth=1,
+                depth=10,
             ),
         )
 
-        self.pose_visualizer_service = node.create_service(
-            PoseArray,
-            "/pose_visualizer",
-            self.visualize_pose_callback,
+        self.pose_visualizer_service = self.create_service(
+            VisualizePoses, "visualize_poses", self.visualize_poses_callback
         )
+
+        self.pose_array = PoseArray()
+
+    def visualize_poses_callback(self, request, response):
+        self.pose_array.poses = request.poses
+        self.pose_array.header.frame_id = request.frame_id
+        self.pose_array.header.stamp = self.get_clock().now().to_msg()
+        self.publisher.publish(self.pose_array)
+        response.success = True
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    visualize_pose_srv = VisualizePoseSrv()
+    rclpy.spin(visualize_pose_srv)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
